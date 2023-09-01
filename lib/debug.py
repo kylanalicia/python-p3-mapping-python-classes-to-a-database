b0a514984550d9ec1a8ba2e10270935d82f052ca
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-09-01 11:21:28 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,14 @@
 from config import CONN, CURSOR
 from song import Song
 
+def reset_database():
+    # Song.drop_table()
+    Song.create_table()
+    Song.create("Hello", "25")
+    Song.create("99 Problems", "The Black Album")
+
+
+reset_database()
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,69 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    all = []
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+       
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+        CONN.commit()
+       
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+    
+    @classmethod
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
+
+        return song
+
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+
+        all = CURSOR.execute(sql).fetchall()
+
+        cls.all = [cls.new_from_db(row) for row in all]
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM songs
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        song = CURSOR.execute(sql, (name,)).fetchone()
+
+        return cls.new_from_db(song)
